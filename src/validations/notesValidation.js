c53943db8f74c01fae103349b0fb8a57e79b781e
Anastasia Tkachova <oracleofmiracle@gmail.com>
2026-07-14 16:52:50 +0300

    fix: make tag, title, and content fields optional in validation schemas

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -17,8 +17,8 @@ export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS),
-    search: Joi.string().trim().allow("")
+    tag: Joi.string().valid(...TAGS).optional(),
+    search: Joi.string().trim().allow("").optional()
   }),
 };
 
@@ -45,8 +45,8 @@ export const updateNoteSchema = {
     noteId: Joi.string().custom(objectIdValidator).required(),
   }),
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).max(30),
-    content: Joi.string().max(65),
-    tag: Joi.string().valid(...TAGS),
+    title: Joi.string().min(1).max(30).optional(),
+    content: Joi.string().max(65).allow("").optional(),
+    tag: Joi.string().valid(...TAGS).optional(),
   }).min(1),
 };
