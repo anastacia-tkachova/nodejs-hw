d622b338d3c31b3548522159d4818236bdd2ed6d
Anastasia Tkachova <oracleofmiracle@gmail.com>
2026-07-14 15:48:46 +0300

    feat: add validation and pagination for notes, integrate celebrate and joi

---
+++ src/validations/notesValidation.js
@@ -0,0 +1,57 @@
+import { Joi, Segments } from 'celebrate';
+import { TAGS } from '../constants/tags.js';
+
+import { isValidObjectId } from 'mongoose';
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid({ TAGS }),
+    search: Joi.string().trim().allow("")
+  }),
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).max(30).required().messages({
+      "string.base": "Title must be a string",
+      "string.min": "Title should have at least {#limit} characters",
+      "string.max": "Title should have at most {#limit} characters",
+      "any.required": "Title is required",
+    }),
+    content: Joi.string().max(65).messages({
+      "string.max": "Content should have at most {#limit} characters",
+    }),
+    tag: Joi.string().valid({ TAGS }).messages({
+      "any.only": "Tag must be one of: 'Work','Personal','Meeting','Shopping','Ideas','Travel','Finance','Health','Important','Todo'",
+    }),
+  }),
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(noteIdSchema).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).max(30),
+    content: Joi.string().max(65),
+    tag: Joi.string().valid({ TAGS }),
+  }).min(1),
+};
+
+// {
+//   [Segments.PARAMS]: Joi.object({
+//     category: Joi.string().valid('work', 'study', 'personal').required(),
+//   })
+// }
